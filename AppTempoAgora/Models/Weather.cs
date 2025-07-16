40b39e1ad9c5237dcdc87fa3e5d0fd7ad168a86e
GuFerreiraV <gustavoferreiravargens@gmail.com>
2025-07-16 14:10:09 -0300

    Adjust

--- AppTempoAgora/Models/Weather.cs
+++ AppTempoAgora/Models/Weather.cs
@@ -2,15 +2,15 @@
 {
     public class Weather
     {
-        public double Lon { get; set; } // Longitude 
-        public double Lat { get; set; } // Latitude
-        public int Visibility { get; set; } // Visibilidade em metros
-        public double Min_temp { get; set; } // Temperatura mínima
-        public double Max_temp { get; set; } // Temperatura máxima
-        public int Sunrise { get; set; } // Hora do nascer do sol (timestamp)
-        public int Sunset { get; set; } // Hora do pôr do sol (timestamp)
-        public string Description { get; set; } // Descrição
-        public string main { get; set; } // resumo das informações do tempo
-        public double speed { get; set; } // velocidade do vento
+        public double? Lon { get; set; } // Longitude 
+        public double? Lat { get; set; } // Latitude
+        public int? Visibility { get; set; } // Visibilidade em metros
+        public double? Min_temp { get; set; } // Temperatura mínima
+        public double? Max_temp { get; set; } // Temperatura máxima
+        public string? Sunrise { get; set; } // Hora do nascer do sol (timestamp)
+        public string? Sunset { get; set; } // Hora do pôr do sol (timestamp)
+        public string? Description { get; set; } // Descrição
+        public string? main { get; set; } // resumo das informações do tempo
+        public double? speed { get; set; } // velocidade do vento
     }
 }
