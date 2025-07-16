a5f53534ed0c1eba1dcc48d87bd75825947da2b9
GuFerreiraV <gustavoferreiravargens@gmail.com>
2025-07-16 14:11:32 -0300

    first steps

--- AppTempoAgora/MainPage.xaml.cs
+++ AppTempoAgora/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace AppTempoAgora
+﻿using AppTempoAgora.Models;
+using AppTempoAgora.Services;
+
+namespace AppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
@@ -9,16 +12,43 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e)
         {
-            count++;
+            try
+            {
+                if (!string.IsNullOrEmpty(txt_city.Text))
+                {
+                    Weather? weather = await DataServices.GetWeather(txt_city.Text);
+                    if (weather != null) 
+                    {
+                        string forecast = "";
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                        forecast = $"Cidade: {txt_city.Text}\n" +
+                                   $"Temperatura Mínima: {weather.Min_temp}°C\n" +
+                                   $"Temperatura Máxima: {weather.Max_temp}°C\n" +
+                                   $"Descrição: {weather.Description}\n" +
+                                   $"Velocidade do Vento: {weather.speed} m/s\n" +
+                                   $"Visibilidade: {weather.Visibility} metros\n" +
+                                   $"Nascer do Sol: {weather.Sunrise}\n" +
+                                   $"Pôr do Sol: {weather.Sunset}";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                        lbl_res.Text = forecast;
+                    }
+                    else 
+                    {
+                        lbl_res.Text = "Sem dados de previsão";
+                    }
+                }
+                else 
+                {
+                    lbl_res.Text = "Preencha a cidade";
+                }
+                 
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
         }
     }
 
